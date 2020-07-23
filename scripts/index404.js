f534f561c41cb6ba3d9c4c4365f30acab65bdf1b
Medis232 <drebuliukas@gmail.com>
2020-07-23 21:45:37 +0300

    lil fix on 404

--- scripts/index404.js
+++ scripts/index404.js
@@ -5,7 +5,7 @@ function btnLink(type) {
     case 2:
       window.location.href = window.location.origin+'/discord'
     case 3:
-      window.location.href = 'https://discord.gg/zG83r6M'
+      window.location.href = 'https://discord.com/oauth2/authorize?client_id=470180228908449792&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FzG83r6M&scope=bot'
   }
 }
 
