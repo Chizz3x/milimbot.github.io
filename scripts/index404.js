9d3227c873997e09b46f9f26d8e0704bde5caa37
Medis232 <drebuliukas@gmail.com>
2020-07-24 17:41:03 +0300

    404 link fixes

--- scripts/index404.js
+++ scripts/index404.js
@@ -1,11 +1,11 @@
 function btnLink(type) {
   switch(type) {
     case 1:
-      window.location.href = window.location.origin
+      return window.location.href = window.location.origin
     case 2:
-      window.location.href = window.location.origin+'/discord'
+      return window.location.href = window.location.origin+'/discord'
     case 3:
-      window.location.href = 'https://discord.com/oauth2/authorize?client_id=470180228908449792&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FzG83r6M&scope=bot'
+      return window.location.href = 'https://discord.com/oauth2/authorize?client_id=470180228908449792&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FzG83r6M&scope=bot'
   }
 }
 
